395e91ceadf4cbb107569d14bd3e3bfb3d90d9e3
fabian.acero <acero01@gmail.com>
2016-10-03 15:07:06 -0500

    Modifico index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+	ini_set("display_errors",1);
+	error_reporting(E_ALL);
 	print("Trying to connect to postgres ... ");
-	$connect = pq_connect("host=172.17.0.3 dbname=postgres port=5432 user=postgres password=C4mb10") or die("Cannot connect to postgres");
-	print_r(var_export($connect,true));
+	$connect = pg_connect("host=172.17.0.3 dbname=postgres port=5432 user=postgres password=C4mbi0") or die("No se pudo");
+	$stat = pg_connection_status($connect);
+
+	if ($stat === PGSQL_CONNECTION_OK) {
+		print("Connection ok " . pg_get_pid($connect));
+	} else {
+		print("Connection error");
+	}
