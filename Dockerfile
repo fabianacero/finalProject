70ca152d56418db2b2d2f6c1c39fce802bb30b75
fabian.acero <acero01@gmail.com>
2016-10-04 11:48:17 -0500

    Primera vesion de lanzamiento funcionando

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,14 @@ LABEL NGINX con PGSQL
 
 # Creando vhost
 ENV DOMAIN_NAME	fabianacero
-RUN mkdir /etc/nginx/sites-available; mkdir /etc/nginx/sites-enabled
+RUN mkdir -p /etc/nginx/sites-available; mkdir -p /etc/nginx/sites-enabled
 ADD vhost.conf /etc/nginx/sites-available/$DOMAIN_NAME.conf
-RUN ln -s /etc/nginx/sites-available/$DOMAIN_NAME.conf /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
+RUN ln -sf /etc/nginx/sites-available/$DOMAIN_NAME.conf /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
 RUN cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkp
-RUN cat /etc/nginx/nginx.conf.bkp | sed 's/\/etc\/nginx\/conf\.d\/\*\.conf;/\/etc\/nginx\/sites-enabled\/\*\.conf;\n    server_names_hash_bucket_size 64;/g' > /etc/nginx/nginx.conf
+RUN cat /etc/nginx/nginx.conf.bkp | sed 's/\/etc\/nginx\/conf\.d\/\*\.conf;/\/etc\/nginx\/conf\.d\/\*\.conf;\ninclude\t\/etc\/nginx\/sites-enabled\/\*\.conf;\n    server_names_hash_bucket_size 64;/g' > /etc/nginx/nginx.conf
 
 # Configurando postgres
 ENV PG_PASSWD=C4mbi0
 RUN yum install php-pgsql -y
 ADD index.php /app/index.php
-ADD ndex.php  /usr/share/nginx/html/index.php
+ADD index.php  /usr/share/nginx/html/index.php
--- index.php
+++ index.php
@@ -1,16 +1,75 @@
+<html>
 <?php
 	ini_set("display_errors",1);
 	error_reporting(E_ALL);
-	print("Trying to connect to postgres ... ");
+	define("PG_USER", "postgres");
 	define("PG_PASSWD", getenv('PG_PASSWD'));
 	define("PG_HOST", "pgdb");
+	define("PG_DB", "postgres");
+	define("PG_PORT", 5432);
+?>
+<head>
+	<title>Proyecto Final Docker!</title>
+	<style type="text/css">
+		.container{
+			margin: 50px auto;
+			width: 300px;
+		}
+		.header{
+		    background-color: #03A9F4;
+		    color: white;
+		    font-weight: bold;
+		    height: 30px;
+		    line-height: 30px;
+		    padding: 0px 10px;
+		    vertical-align: middle;
+		    width: 250px;
+		}
+		.result{
+	    	background-color: #c6dbea;
+    		padding: 6px 10px;
+    		width: 250px;
+		}
+		.result p {
+			margin: 0px;
+		}
+		.score{
+	    	background-color: #f39647;
+    		font-weight: bold;
+	    	padding: 6px 10px;
+    		width: 250px;
+		}
+		.score p {
+			margin: 0px;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="header" >Trying to connect to postgres ...</div>
+		<?php 
+			$connect = pg_connect("host=" . PG_HOST . " dbname=" . PG_DB . " port=" . PG_PORT . " user=" .PG_USER . " password=" . PG_PASSWD ) 
+				or die("Couln't connect with postgres db. Verify your connection params");
+			$stat = pg_connection_status($connect);
+		?>
 
-	$connect = pg_connect("host=" . PG_HOST . " dbname=postgres port=5432 user=postgres password=" . PG_PASSWD ) 
-		or die("couln't connect with postgres db. Verify your connection params");
-	$stat = pg_connection_status($connect);
-
-	if ($stat === PGSQL_CONNECTION_OK) {
-		print("Connection ok " . pg_get_pid($connect));
-	} else {
-		print("Connection error");
-	}
+		<?php if ($stat === PGSQL_CONNECTION_OK): ?>
+			<div class="result">
+				<p>Connection ok</p>
+				<p>Successfully connected to <strong><?= PG_HOST ?></strong> host</p>
+				<p>Status:<?= pg_get_pid($connect) ?></p>
+				<p>Resource: <?= $connect ?></p>
+			</div>
+			<div class="score">
+				<p>Writing exam value : 0.5%</p>
+				<p>Exercise value : 99.5%</p>
+			</div>
+		<?php else: ?>
+			<div class="result">
+				<p>Connection error</p>
+				<p>Error:<?= pg_last_error($connect) ?></p>
+			</div>
+		<?php endif; ?>
+	</div>
+</body>
+</html>
